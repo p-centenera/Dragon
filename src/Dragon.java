3d1c4652d726f8601a8bb79645a8156af3cf38dc
p-centenera <100157069+p-centenera@users.noreply.github.com>
2022-03-24 14:04:21 +0100

    Update Dragon.java

--- src/Dragon.java
+++ src/Dragon.java
@@ -18,13 +18,53 @@ public class Dragon extends GTurtle{
             Inversa(n-1);
        }
     }
-  public void run() {
+
+    void H(int n){
+       if (n>0){
+         
+           left(90);
+           HI(n-1);
+           right(90);
+           forward(5);
+           H(n-1);
+           right(90);
+           forward(5);
+           left(90);
+           H(n-1);
+           right(180);
+           forward(5);
+           left(90);
+           HI(n-1);
+           left(90);
+       }
+   } 
+    void HI(int n){
+       if {
+           right(90);
+           H(n-1);
+           left(90);
+           forward(5);
+           HI(n-1);
+           left(90);
+           forward(5);
+           right(90);
+           HI(n-1);
+           left(180);
+           forward(5);
+           right(90);
+           H(n-1);
+           right(90);       
+           }
+    }  
+    
+    public void run() {
          // Establecer el tamaño (en píxeles) de la tortuga.
         setSize(10);
         // Bajar la pluma de la tortuga para que deje un
         // rastro al momento de desplazarse.
         penDown();
-        Directa(8);
+        //Directa(8);
+        H(3);
  } 
    public static void main(String[] args) {
         // TODO code application logic here
